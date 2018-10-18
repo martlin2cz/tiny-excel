d39b6f644e6b2adbac6aac62c13ea2865756729d
m@rtlin2cz <martlin2cz@gmail.com>
2018-10-18 18:16:46 +0200

    Initial try of gsheets
     - unsuccessfull

---
+++ src/main/java/cz/martlin/sheets2csv/TestAppv1.java
@@ -0,0 +1,12 @@
+package cz.martlin.sheets2csv;
+
+
+@Deprecated
+public class TestAppv1 {
+//  public static void main(String[] args)
+//      throws AuthenticationException, MalformedURLException, IOException, ServiceException {
+//
+//    // Application code here
+//
+//  }
+}
